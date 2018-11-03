dfec10056c827a9e96dc0e2913dd1c65029167f0
Jorge Serras <caveiroserras@gmail.com>
2018-11-03 02:29:23 +0000

    instructions

---
+++ OutlierDetection/README.txt
@@ -0,0 +1,8 @@
+
+Please follow the instructions at 
+
+https://jorgeserras.github.io/MTS_OutlierDetection/
+
+Online version of the application:
+
+https://jorgeserras.shinyapps.io/outlierdetection/
---
+++ OutlierDetection/installation.R
@@ -0,0 +1,6 @@
+
+## Run this script once to install all required dependencies
+
+## Automatically installs and loads the required packages
+ if(!require('pacman'))install.packages('pacman')
+ pacman::p_load(shiny, rJava, here, digest, shinyjs,dplyr,DiagrammeR,shinythemes, shinycustomloader, bsplus, reshape2, ggplot2, gsubfn, jmotif, qdapTools, mclust) 
